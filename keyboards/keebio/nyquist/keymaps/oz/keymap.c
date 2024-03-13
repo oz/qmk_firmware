f87c53c21b5f4107da7096d7f34c2eeba0ae20de
Arnaud Berthomier <oz@cyprio.net>
2024-03-13 12:07:39 -0600

    Playing with tap modifiers on home-row

--- keyboards/keebio/nyquist/keymaps/oz/keymap.c
+++ keyboards/keebio/nyquist/keymaps/oz/keymap.c
@@ -3,11 +3,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // QWERTY layer
 [0] = LAYOUT_ortho_5x12(
-  KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,  KC_5,           KC_6,           KC_7,  KC_8,    KC_9,    KC_0,    KC_BSLS,
-  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,           KC_Y,           KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,           KC_H,           KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,           KC_N,           KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
-  KC_LCTL,        KC_RALT, KC_LALT, KC_LGUI, MO(2), LALT_T(KC_SPC), RGUI_T(KC_SPC), MO(3), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+KC_GRV,          KC_1,          KC_2,          KC_3,          KC_4,          KC_5,            KC_6,            KC_7,          KC_8,          KC_9,          KC_0,             KC_BSLS,
+KC_TAB,          KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,            KC_Y,            KC_U,          KC_I,          KC_O,          KC_P,             KC_BSPC,
+LCTL_T(KC_ESC),  LGUI_T(KC_A),  LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),  KC_G,            KC_H,            LSFT_T(KC_J),  LCTL_T(KC_K),  RALT_T(KC_L),  RGUI_T(KC_SCLN),  KC_QUOT,
+KC_LSFT,         KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,            KC_N,            KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,          SC_SENT,
+KC_LCTL,         KC_RALT,       KC_LALT,       KC_LGUI,       MO(2),         LALT_T(KC_SPC),  RGUI_T(KC_SPC),  MO(3),         KC_LEFT,       KC_DOWN,       KC_UP,            KC_RGHT
 ),
 
 // Colemak
