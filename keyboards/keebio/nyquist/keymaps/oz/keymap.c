bc933426cdd2604c77c9da2788338f3e3d377c54
Arnaud Berthomier <oz@cyprio.net>
2025-07-17 12:11:14 -0600

    wip: More tweaks to this keeb.
    Mostly committing this to be able to rebase the branch the lastest
    upstream.

--- keyboards/keebio/nyquist/keymaps/oz/keymap.c
+++ keyboards/keebio/nyquist/keymaps/oz/keymap.c
@@ -51,14 +51,34 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 
+/*
+ * Some notes about this weird layout of mine...
+ *
+ *  For better of for worse, I'm super used to having homerow mods,
+ *  under both hands, in that order: LGUI, LALT, LCTRL, LSFT. Mirrored
+ *  on the right half.
+ *
+ *  Right-alt is my compose key on macos, used to make accentuated chars
+ *  and such.
+ *
+ *  The bottom row is a bit of a mess / work-in-progress for now.
+ *
+ *  Layer 2 is mostly ununsed. I should remedy that.
+ *
+ *  Layer 3 is activated using my right thumb, and contains symbols. Low
+ *  key inspired by Miryoku.
+ *
+ *  Layer 5 is the mouse mostly. I use my left palm to hold the modifier
+ *  down, and right hand to control the mouse. Works for me!
+ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // QWERTY layer
 [0] = LAYOUT_ortho_5x12(
-  TD(TD_TILDE_CW),  KC_1,          KC_2,          KC_3,          KC_4,          KC_5,            KC_6,            KC_7,          KC_8,          KC_9,            KC_0,             KC_BSLS,
-  KC_TAB,           KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,            KC_Y,            KC_U,          KC_I,          KC_O,            KC_P,             KC_BSPC,
-  LCTL_T(KC_ESC),   LGUI_T(KC_A),  LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),  KC_G,            KC_H,            RSFT_T(KC_J),  LCTL_T(KC_K),  LALT_T(KC_L),    RGUI_T(KC_SCLN),  KC_QUOT,
-  KC_LSFT,          KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,            KC_N,            KC_M,          KC_COMM,       RALT_T(KC_DOT),  KC_SLSH,          SC_SENT,
-  MO(5),            KC_RALT,       KC_LALT,       KC_LGUI,       MO(2),         LALT_T(KC_ENT),  RGUI_T(KC_SPC),  MO(3),         KC_LEFT,       KC_DOWN,         KC_UP,            KC_RGHT
+    TD(TD_TILDE_CW),  KC_1,          KC_2,          KC_3,          KC_4,            KC_5,           KC_6,            KC_7,          KC_8,          KC_9,            KC_0,             KC_BSLS,
+    KC_TAB,           KC_Q,          KC_W,          KC_E,          KC_R,            KC_T,           KC_Y,            KC_U,          KC_I,          KC_O,            KC_P,             KC_BSPC,
+    LCTL_T(KC_ESC),   LGUI_T(KC_A),  LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),    KC_G,           KC_H,            RSFT_T(KC_J),  LCTL_T(KC_K),  LALT_T(KC_L),    RGUI_T(KC_SCLN),  KC_QUOT,
+    KC_LSFT,          KC_Z,          KC_X,          KC_C,          KC_V,            KC_B,           KC_N,            KC_M,          KC_COMM,       RALT_T(KC_DOT),  KC_SLSH,          SC_SENT,
+    MO(5),            KC_RALT,       KC_LALT,       KC_LGUI,       RGUI_T(KC_ENT),  LT(2, KC_ENT),  LT(3, KC_SPC),   KC_BSPC,       KC_LEFT,       KC_DOWN,         KC_UP,            KC_RGHT
 ),
 
 // Colemak
@@ -67,25 +87,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,  KC_Q,          KC_W,          KC_F,          KC_P,           KC_B,     KC_J,     KC_L,          KC_U,          KC_Y,          KC_SCLN,       KC_TRNS,
   KC_TRNS,  LGUI_T(KC_A),  LALT_T(KC_R),  LCTL_T(KC_S),  LSFT_T(KC_T),   KC_G,     KC_M,     RSFT_T(KC_N),  LCTL_T(KC_E),  LALT_T(KC_I),  RGUI_T(KC_O),  KC_TRNS,
   KC_TRNS,  KC_Z,          KC_X,          KC_C,          KC_D,           KC_V,     KC_K,     KC_H,          KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,
-  KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,       MO(2),          KC_TRNS,  KC_TRNS,  MO(3),         KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
+  KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
 ),
 
-// Symbols
+// Functions
 [2] = LAYOUT_ortho_5x12(
-  KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC,
-  KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
-  KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_BSPC,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,
-  KC_TRNS,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_STOP,  KC_MPLY,  KC_MNXT,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   MO(4),   KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END
+    KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC,
+    KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
+    KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_BSPC,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,
+    KC_TRNS,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_STOP,  KC_MPLY,  KC_MNXT,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  MO(4),    KC_TRNS,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END
 ),
 
 // Symbols
 [3] = LAYOUT_ortho_5x12(
-  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
-  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
-  KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
-  KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,  KC_F12,   KC_TRNS,  KC_TRNS,  KC_MUTE,  KC_VOLD,  KC_VOLU,
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  MO(4),   KC_TRNS, KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END
+    KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  LSFT(KC_COMM),  LSFT(KC_DOT),  KC_LPRN,        KC_RPRN,        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_MINS,        KC_EQL,        KC_LBRC,        KC_RBRC,        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PIPE,
+    KC_TRNS,  KC_TRNS,  KC_UNDS,        KC_PLUS,       LSFT(KC_LBRC),  LSFT(KC_RBRC),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_TRNS,        MO(4),          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 
 // Keyboard settings
